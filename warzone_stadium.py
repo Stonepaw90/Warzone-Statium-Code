0880c91718df37510346d8488b6aaeb9049c212c
Stonepaw90 <75340597+Stonepaw90@users.noreply.github.com>
2021-01-29 10:42:42 -0500

    Update warzone_stadium.py

--- warzone_stadium.py
+++ warzone_stadium.py
@@ -135,6 +135,10 @@ def main():
         else:
             finalstring = string3
     st.header("Your code is: " + finalstring)
+    log_file = open("log.txt", "w")
+    log_file.write(user_code1 + " " + usercode_2 + " " + user_code3 + " :" + finalstring)
+    log_file.close()
+    
     code(finalstring)
 if __name__ == "__main__":
     main()
