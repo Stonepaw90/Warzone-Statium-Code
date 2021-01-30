2a2d2b4a3dab13542cefbcc4ef5b9fa6a52c5409
Stonepaw90 <75340597+Stonepaw90@users.noreply.github.com>
2021-01-30 10:52:08 -0500

    Update warzone_stadium.py

--- warzone_stadium.py
+++ warzone_stadium.py
@@ -1,71 +1,5 @@
 import streamlit as st
 import logging
-import pandas as pd
-from pathlib import Path
-import sqlite3
-from sqlite3 import Connection
-
-URI_SQLITE_DB = "test.db"
-
-
-def main1():
-    st.title("My Super Calculator")
-    st.markdown("Enter data in database from sidebar, then run the **mighty** calculator")
-    
-    conn = get_connection(URI_SQLITE_DB)
-    init_db(conn)
-
-    build_sidebar(conn)
-    display_data(conn)
-    run_calculator(conn)
-
-
-def init_db(conn: Connection):
-    conn.execute(
-        """CREATE TABLE IF NOT EXISTS test
-            (
-                INPUT1 INT,
-                INPUT2 INT
-            );"""
-    )
-    conn.commit()
-
-
-def build_sidebar(conn: Connection):
-    st.sidebar.header("Configuration")
-    input1 = st.sidebar.slider("Input 1", 0, 100)
-    input2 = st.sidebar.slider("Input 2", 0, 100)
-    if st.sidebar.button("Save to database"):
-        conn.execute(f"INSERT INTO test (INPUT1, INPUT2) VALUES ({input1}, {input2})")
-        conn.commit()
-
-
-def display_data(conn: Connection):
-    if st.checkbox("Display data in sqlite databse"):
-        st.dataframe(get_data(conn))
-
-
-def run_calculator(conn: Connection):
-    if st.button("Run calculator"):
-        st.info("Run your function")
-        df = get_data(conn)
-        st.write(df.sum())
-
-
-def get_data(conn: Connection):
-    df = pd.read_sql("SELECT * FROM test", con=conn)
-    return df
-
-
-@st.cache(hash_funcs={Connection: id})
-def get_connection(path: str):
-    """Put the connection in cache to reuse if path does not change between Streamlit reruns.
-    NB : https://stackoverflow.com/questions/48218065/programmingerror-sqlite-objects-created-in-a-thread-can-only-be-used-in-that-sa
-    """
-    return sqlite3.connect(path, check_same_thread=False)
-
-
-
 from PIL import Image
 def factorial(n):
     if n == 1:
@@ -185,7 +119,9 @@ def combine(string1, string2):
     return (final)
 def main():
     im = Image.open("enigma.png")
+    
     st.image(im, caption='This Blueprint for the CR-56 AMAX comes after collecting key cards in Stadium.', use_column_width=True)
+    st.header("by Abraham Holleran")
     st.write("Input your code or codes, with N for the nose symbol, H for the house symbol, \
              and S for the squiggly symbol that looks like a dollar sign. This webapp will crack \
              the code for you and will show you the \
@@ -222,4 +158,4 @@ def main():
 
     code(finalstring)
 if __name__ == "__main__":
-    main1()
+    main()
